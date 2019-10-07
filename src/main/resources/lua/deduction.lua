23e4311518bc37a211ddad1cb628c3ff69866672
lchpersonal <326018662@qq.com>
2019-10-07 21:22:04 +0800

    [修改代码]test

--- src/main/resources/lua/deduction.lua
+++ src/main/resources/lua/deduction.lua
@@ -1,7 +1,7 @@
 local key = KEYS[1];
 local deduction = tonumber(ARGV[1]);
 local total = redis.call("hget", key, "total_num");
-local used = redis.call("hget", key, "used_num");
+local used = redis.call("hget", key, "used_num") ;
 if total == nil or used == nil then
     return 0;
 else
